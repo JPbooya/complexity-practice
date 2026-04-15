eb65ea0145533bb09e0e51a2dabbdaf8704ebf5c
JPbooya <perezjcarlos81@gmail.com>
2026-04-15 16:09:10 -0700

    finished all problem sets

--- src/Main.java
+++ src/Main.java
@@ -173,7 +173,7 @@ public class Main {
   // Write this method to efficiently return the corresponding email or "Person not found" if appropriate
   // assume that each String is bounded by a constant length
   // What is the time complexity of your solution?
-  // YOUR ANSWER HERE
+  // O(1)
   public static String emailLookupEfficient(HashMap<String, String> namesToEmails, String queryName) {
     if (namesToEmails.containsKey(queryName)){
       return namesToEmails.get(queryName);
@@ -184,7 +184,7 @@ public class Main {
   // What is the time complexity of this method?
   // assume that each String is bounded by a constant length
   // (assume the set and list have the same number of elements)
-  // YOUR ANSWER HERE
+  // O(n)
   public static boolean hasCommon(HashSet<String> wordSet, ArrayList<String> wordList) {
     for(String word : wordSet) {
       if(wordList.contains(word)) {
@@ -197,8 +197,13 @@ public class Main {
   // Do not change the datatype of wordSet or wordList.
   // assume that each String is bounded by a constant length
   // What is the time complexity of your new solution?
-  // YOUR ANSWER HERE
+  // O(n) because is iterating through n.
   public static boolean hasCommonEfficient(HashSet<String> wordSet, ArrayList<String> wordList) {
+      for (String word : wordList) {
+        if (wordSet.contains(word)) {
+          return true;
+        }
+      }
     return false;
   }
 
@@ -207,14 +212,14 @@ public class Main {
   // The prices will be updated frequently throughout the day, and you need to efficiently update
   // and access the current price for each stock. The order of the ticker symbols is not important.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // HashMap because using a HashMap is O(1) and order does not matter.
 
   // Suppose you are building a music player application where users can create playlists.
   // Songs can be added to the end of the playlist in the order the user chooses, and the user can
   // skip to the next or previous song. Most operations involve adding songs and accessing them by
   // their position in the playlist.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // ArrayList would be the DS to use here with a time complexity of O(1).
 
   // Suppose you are developing a search feature that keeps track of the user's
   // recent search queries. You want to store the queries in the order they were made,
@@ -222,5 +227,5 @@ public class Main {
   // relatively small, and it is more important to preserve the order of the searches than
   // to optimize for fast lookups or deletions.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // ArrayList because order is needed and O(1) for fast look ups.
 }
